12df63a78be6c5dea42a78eef712b86878a7255d
Li Fangyu <ccccces@163.com>
2025-12-08 15:22:52 +0800

    Remove copyright headers from client patch files
    - HealthPatch.cs
    - Health_HurtPacth.cs
    - SlotPatch.cs
    - ItemPatch.cs
    - GunPatch.cs
    - DoorPatch.cs
    - ItemUtilitiesPatch.cs
    - InteractableLootboxPatch.cs
    - CharacterMainControlPatch.cs

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -5,5 +5,5 @@ internal static class BuildInfo
     internal const string Name = "EscapeFromDuckovCoopMod";
     internal const string Copyright = "Copyright  2025";
     internal const string ModVersion = "2.0.2.1";
-    internal const string CommitHash = "fatal: not a git repository (or any of the parent directories): .git";
+    internal const string CommitHash = "45be3bc";
 }
